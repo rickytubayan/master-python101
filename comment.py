1241bb522b3df00b960cd98c37ea9eb890e87acc
rickytubayan <rickytubayan@gmail.com>
2025-09-29 11:46:01 +0800

    fixing comments

--- comment.py
+++ comment.py
@@ -1,7 +1,7 @@
 # This is a comment 
 # and this is the second comment
 # ... and now a third!
-"# This a comment because it's inside quotes."
+"# This is not a comment because it's inside quotes."
 """
 This is a multi line comment.
 """
