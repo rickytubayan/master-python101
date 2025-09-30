2bab6f3324f6836a31e3aaf54bc0accbf8345b8b
Ricky Tubayan <ricky.tubayan@gmail.com>
2025-09-30 09:25:20 +0800

    invalid variable names

--- naming_variables.py
+++ naming_variables.py
@@ -1,3 +1,15 @@
 #Valid Naming of Variables
 x = True
 _x = True
+
+#Invalid Naming of Variables
+
+9x = True
+#Dont Start with Numerals
+
+$x = False
+#Dont Start with Symbols
+
+"""
+Invalid Variable Naming Will Output Syntax Errors
+"""
