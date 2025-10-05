f4721b7d81933adf68ae00eada185becc4cbd677
Ricky Tubayan <ricky.tubayan@gmail.com>
2025-10-05 08:02:18 +0800

    list count from last element

--- list.py
+++ list.py
@@ -4,3 +4,9 @@ list2 = ['one',2,3,'Four',(5,6), [7,8,9]]
 list2[2] = 'newvalue'
 
 print(list2)
+
+#List Count from Last Element
+list3 = ['Allen','Mae','Juliet','Max','Jake']
+
+print(list3[-1])
+print(list3[-4])
